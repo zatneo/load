36c5cb97c396f66aa281723d54b75618a10b0057
zatneo <zatneosoft@gmail.com>
2024-08-28 12:42:21 +0530

    Test Changes

--- src/test/java/com/testing/load/LoadApplicationTests.java
+++ src/test/java/com/testing/load/LoadApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class LoadApplicationTests {
 
 	@Test
